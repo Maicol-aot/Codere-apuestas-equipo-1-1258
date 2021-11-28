aabf1d84988b099a6390f2dd8d39204805406992
hevertjz <hevertjz@gmail.com>
2021-11-28 11:36:11 -0500

    implementacion dashbboard

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"; //pendiente por utilizar la libreria en reactdDOM.
 
 ReactDOM.render(
   <React.StrictMode>
