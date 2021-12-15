beed4d15b6676b9abb4137aa37dbadc67879d997
hevertjz <hevertjz@gmail.com>
2021-12-15 07:35:15 -0500

    ajuste  en formulario registro

--- src/forms/CrearUsuario.js
+++ src/forms/CrearUsuario.js
@@ -37,7 +37,7 @@ const CrearUsuario = () =>{
                             <option selected>Tipo de documento</option>
                             <option value="1">Cédula de ciudadania</option>
                             <option value="2">Cédula de extranjería</option>
-                            <option value="3">Pasaporte</option>
+                            
                         </select>
 
                     </div>
@@ -64,13 +64,63 @@ const CrearUsuario = () =>{
                         <input type="text" className="form-control" id="validationDefault01" placeholder="Correo electronico" required />
                     </div>
                     <div className="col-sm-3 mb-3">
-
-                        <input type="text" className="form-control" id="validationDefault02" placeholder="Pais de residencia" required />
+                    <select className="form-select" id="select-residencia" formcontrolname="diaNacimiento" aria-label="Default select example" required="true">
+                                <option selected>Departamento</option>
+                                <option value="1">Amazonas</option>
+                                <option value="2">Antioquia</option>
+                                <option value="3">Arauca</option>
+                                <option value="4">Atlántico</option>
+                                <option value="5">Bogotá</option>
+                                <option value="6">Bolívar</option>
+                                <option value="7">Boyacá</option>
+                                <option value="8">Caldas</option>
+                                <option value="9">Caquetá</option>
+                                <option value="10">Casanare</option>
+                                <option value="11">Cauca</option>
+                                <option value="12">Cesar</option>
+                                <option value="13">Chocó</option>
+                                <option value="14">Córdoba</option>
+                                <option value="15">Cundinamarca</option>
+                                <option value="16">Guainía</option>
+                                <option value="17">Guaviare</option>
+                                <option value="18">Huila</option>
+                                <option value="19">La Guajira</option>
+                                <option value="20">Magdalena</option>
+                                <option value="21">Meta</option>
+                                <option value="22">Nariño</option>
+                                <option value="23">Norte de Santander</option>
+                                <option value="24">Putumayo</option>
+                                <option value="25">Quindío</option>
+                                <option value="26">Risaralda</option>
+                                <option value="27">San Andrés y Providencia</option>
+                                <option value="28">Santander</option>
+                                <option value="29">Sucre</option>
+                                <option value="30">Tolima</option>
+                                <option value="31">Valle del Cauca</option>
+                                <option value="32">Vaupés</option>
+                                <option value="33">Vichada</option>
+                            
+                            </select> 
                     </div>
+
+                    <div className="col-sm-3 mb-3">
+
+                        <input type="text" className="form-control" id="validationDefault02" placeholder="Ciudad" required />
+                        
+                    </div>
+
+                    <div className="col-sm-3 mb-3">
+
+                    <input className="form-control" id="form-control" type="text" placeholder="Direccion" name="direccion" required="true"/>
+                        
+                    </div>
+                    
+
                     <div className="col-sm-3 mb-3">
 
                         <input type="text" className="form-control" id="validationDefault02" placeholder="Teléfono" required />
                     </div>
+
 
 
 
