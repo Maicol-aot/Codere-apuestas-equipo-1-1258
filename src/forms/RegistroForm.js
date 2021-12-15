34e4b48e60dee63243cbb23e534ad1dc817be6f7
hevertjz <hevertjz@gmail.com>
2021-12-15 15:07:40 -0500

    validaciones minimas registro

--- src/forms/RegistroForm.js
+++ src/forms/RegistroForm.js
@@ -16,92 +16,92 @@ const RegistroForm = () => {
         if (conPassword !== password) return alert('Las contraseñas no son iguales'); 
     };
 
-    // <div> intento validaciones minimas
-    //     conflicto con ids form-controls
-    // const validarEmail = () =>{
-    //     let infoEmail = document.getElementById('email2').value;
-    //     let formato_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
-    //     if (!infoEmail.match(formato_email)) {
-    //         return alert('Por favor escriba un correo electronico válido.');
-    //     }
-    // };
+
+    const validarNombre = () =>{
+            let infoNombre = document.getElementById('nombre').value;
+            let fomrato_texto = /([a-zA-Z]{3,30}\s*)+/;
+            if (!infoNombre.match(fomrato_texto)) {
+                return alert('Por favor ingresa un dato valido en la casilla "Nombre" '); //cambiar el alert por una validacion de errores general porque envia los datoss
+            }
+        };
+
+    
+
+    const validarEmail = () =>{
+        let infoEmail = document.getElementById('email2').value;
+        let formato_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+        if (!infoEmail.match(formato_email)) {
+            return alert('Por favor escriba un correo electronico válido.');
+        }
+    };
      
-    // const validarEdad = () =>{
-    //     let infoEdad = document.getElementById('nacimiento').value;
-    //     let limiteEdad = 2003-12-15;
-    //     console.log(infoEdad);
-    //     if (parseInt(infoEdad) > limiteEdad) {
-    //         return alert('Tienes que ser mayor de 18 años para  registrarte'); //cambiar el alert por una validacion de errores general porque envia los datoss
-    //     }
-    // };
+    const validarEdad = () =>{
+        let infoEdad = document.getElementById('nacimiento').value;
+        let limiteEdad = 2003-12-15;
+        console.log(infoEdad);
+        if (parseInt(infoEdad) > limiteEdad) {
+            return alert('Tienes que ser mayor de 18 años para  registrarte'); //cambiar el alert por una validacion de errores general porque envia los datoss
+        }
+    };
 
-    // const validarNombre = () =>{
-    //     let infoNombre = document.getElementsByName('nombre').value;
-    //     let fomrato_texto = /([a-zA-Z]{3,30}\s*)+/;
-    //     if (!infoNombre.match(fomrato_texto)) {
-    //         return alert('Por favor ingresa un dato valido en la casilla "Nombre" '); //cambiar el alert por una validacion de errores general porque envia los datoss
-    //     }
-    // };
 
-    // const validarApellido1 = () =>{
-    //     let infoApellido1 = document.getElementById('apellido1').value;
-    //     let fomrato_texto = /([a-zA-Z]{3,30}\s*)+/;
-    //     if (!infoApellido1.match(fomrato_texto)) {
-    //         return alert('Por favor ingresa un dato valido en la casilla "Primer Apellido" ');
+    const validarApellido1 = () =>{
+        let infoApellido1 = document.getElementById('apellido1').value;
+        let fomrato_texto = /([a-zA-Z]{3,30}\s*)+/;
+        if (!infoApellido1.match(fomrato_texto)) {
+            return alert('Por favor ingresa un dato valido en la casilla "Primer Apellido" ');
 
-    //     }
-    // };
+        }
+    };
     
-    // const validarApellido2 = () =>{
-    //     let infoApellido2 = document.getElementById('apellido2').value;
-    //     let fomrato_texto = /([a-zA-Z]{3,30}\s*)+/;
-    //     if (!infoApellido2.match(fomrato_texto)) {
-    //         return alert('Por favor ingresa un dato valido en la casilla "Segundo Apellido" ');
+    const validarApellido2 = () =>{
+        let infoApellido2 = document.getElementById('apellido2').value;
+        let fomrato_texto = /([a-zA-Z]{3,30}\s*)+/;
+        if (!infoApellido2.match(fomrato_texto)) {
+            return alert('Por favor ingresa un dato valido en la casilla "Segundo Apellido" ');
 
-    //     }
-    // };
+        }
+    };
 
-    // const validarNdoc = () =>{
-    //     let ndoc = document.getElementById('ndoc').value;
-    //     let formato_numero = /^([0-9])*$/;
-    //     if (!ndoc.match(formato_numero)){
-    //         return alert('Solo puedes ingresar numeros en el campo "No. de documento"'); //cambiar el alert por una validacion de errores general porque envia los datoss
-    //     }
-    // };
+    const validarNdoc = () =>{
+        let ndoc = document.getElementById('ndoc').value;
+        let formato_numero = /^([0-9])*$/;
+        if (!ndoc.match(formato_numero)){
+            return alert('Solo puedes ingresar numeros en el campo "No. de documento"'); //cambiar el alert por una validacion de errores general porque envia los datoss
+        }
+    };
 
-    // const validarLugarExp = () =>{
-    //     let lugarExp = document.getElementById('lugarExp').value;
-    //     let fomrato_texto = /^[a-z ,.'-]+$/i;
+    const validarLugarExp = () =>{
+        let lugarExp = document.getElementById('lugarExp').value;
+        let fomrato_texto = /^[a-z ,.'-]+$/i;
         
         
-    //     if (!lugarExp.match(fomrato_texto)) {
-    //         return alert('Por favor ingresa un dato valido en la casilla "Lugar de expedicón" ');
+        if (!lugarExp.match(fomrato_texto)) {
+            return alert('Por favor ingresa un dato valido en la casilla "Lugar de expedicón" ');
 
-    //     }
-    // };
+        }
+    };
 
-    // const validarCiudad = () =>{
-    //     let ciudad = document.getElementById('ciudad').value;
-    //     let fomrato_texto = /^[a-z ,.'-]+$/i;
+    const validarCiudad = () =>{
+        let ciudad = document.getElementById('ciudad').value;
+        let fomrato_texto = /^[a-z ,.'-]+$/i;
         
         
-    //     if (!ciudad.match(fomrato_texto)) {
-    //         return alert('Por favor ingresa un dato valido en la casilla "Ciudad" ');
+        if (!ciudad.match(fomrato_texto)) {
+            return alert('Por favor ingresa un dato valido en la casilla "Ciudad" ');
 
-    //     }
-    // };
+        }
+    };
 
-    // const validarTel = () =>{
-    //     let nTel = document.getElementById('tel').value;
-    //     let formato_numero = /^([0-9]{0,10})*$/;
-    //     if (!nTel.match(formato_numero)){
-    //         return alert('ingresa un numero de telefono valido en el campo "Teléfono"'); //cambiar el alert por una validacion de errores general porque envia los datoss
-    //     }
-    // };
-    // fin valicadiones minimas
+    const validarTel = () =>{
+        let nTel = document.getElementById('tel').value;
+        let formato_numero = /^([0-9]{0,10})*$/;
+        if (!nTel.match(formato_numero)){
+            return alert('ingresa un numero de telefono valido en el campo "Teléfono"'); //cambiar el alert por una validacion de errores general porque envia los datoss
+        }
+    };
+   
         
-    // </div>
-    
 
     
     return(
@@ -112,12 +112,12 @@ const RegistroForm = () => {
                     <form className="registerForm">
                         <div className="rows">
                             <h6 className="form-subtitles">Datos personales</h6>
-                            <input className="form-control" id="form-control" type="text" placeholder="Nombre" name="nombre" required="true" />
-                            <input className="form-control" id="form-control"  type="text" placeholder="Primer apellido" name="apellido1" required="true"/>
-                            <input className="form-control" id="form-control" type="text" placeholder="Segundo apellido" name="segundoApellido" required="true"/>
+                            <input className="form-control" id="nombre" type="text" placeholder="Nombre" name="nombre" required="true" onBlur={validarNombre}/>
+                            <input className="form-control" id="apellido1"  type="text" placeholder="Primer apellido" name="PrimerApellido" required="true" onBlur={validarApellido1}/>
+                            <input className="form-control" id="apellido2" type="text" placeholder="Segundo apellido" name="segundoApellido" required="true" onBlur={validarApellido2}/>
                             <div className="cont">
                                 <h6 id="form-subtitles">Fecha nacimiento</h6>
-                                <input type="date" class="form-date" id="fechaNacimiento" required="true" />
+                                <input type="date" className="form-date" id="nacimiento" required="true" onBlur={validarEdad} />
                             </div>
                         
                         </div>
@@ -127,8 +127,8 @@ const RegistroForm = () => {
                                 <option selected value="Cedula de ciudadania">Cedula de ciudadania</option>
                                 <option value="Cedula de extranjeria">Cedula de extranjeria</option>
                             </select> 
-                            <input className="form-control" id="form-control" type="text" placeholder="Numero de documento" name="numeroDocumento" required="true"/>
-                            <input className="form-control" id="form-control" type="text" placeholder="Lugar de expedicion" name="sitioExpedicion" required="true"/>
+                            <input className="form-control" id="ndoc" type="text" placeholder="Numero de documento" name="numeroDocumento" required="true" onBlur={validarNdoc}/>
+                            <input className="form-control" id="lugarExp" type="text" placeholder="Lugar de expedicion" name="sitioExpedicion" required="true" onBlur={validarLugarExp} />
                             <div className="cont">
                                 <h6 id="form-subtitles">Fecha expedicion</h6>
                                 <input type="date" class="form-date" required="true"/>
@@ -174,10 +174,10 @@ const RegistroForm = () => {
                                 <option value="33">Vichada</option>
                             
                             </select> 
-                            <input type="text" className="form-control" id="select-residencia" formcontrolname="diaNacimiento" required="true" placeholder="Muncipio de residencia"/> 
+                            <input type="text" className="form-control" id="ciudad" formcontrolname="diaNacimiento" required="true" placeholder="Muncipio de residencia" onBlur={validarCiudad}/> 
                             <input className="form-control" id="form-control" type="text" placeholder="Direccion de residencia" name="direccion" required="true"/>
-                            <input className="form-control" id="form-control" type="email" placeholder="Correo electronico" name="email" required="true" />
-                            <input className="form-control" id="movil" type="text" placeholder="Movil" name="nombre" /> 
+                            <input className="form-control" id="email2" type="email" placeholder="Correo electronico" name="email" required="true" onBlur={validarEmail}/>
+                            <input className="form-control" id="tel" type="text" placeholder="Movil" name="nombre" onBlur={validarTel}/> 
                         </div>
                         <div className="rows">
                             <h6 className="form-subtitles-single">Datos usuario</h6>
