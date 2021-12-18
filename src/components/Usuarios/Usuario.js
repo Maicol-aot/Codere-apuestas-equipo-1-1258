a176001133174ff531091a73c6528a44579f365f
hevertjz <hevertjz@gmail.com>
2021-12-17 22:56:42 -0500

    error de renderizado corregido

--- src/components/Usuarios/Usuario.js
+++ src/components/Usuarios/Usuario.js
@@ -4,10 +4,10 @@ const Usuario = (props) =>{
     return(
         <>
             <tr>
-                <th scope="row">{props.id}</th>
-                <td>{props.username}</td>
+                <th scope="row">{props.id_usuario}</th>
+                <td>{props.usrname}</td>
                 <td>{props.nombre}  {props.primerApellido}</td>
-                <td>{props.tipouser}</td>
+                <td>{props.rol}</td>
                 <td><td><a href="#" className="btn btn-danger">Eliminar</a></td></td>
             </tr>
         </>
