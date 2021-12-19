3312eb11db8686a7f8e4e114d7652c03a5f130b6
Maicol <maicolr@uninorte.edu.co>
2021-12-18 22:29:49 -0500

    Ingreso de usuarios a la pagina web (falta arreglar temas de roles)

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import LandingPage from '../pages/LandingPage';
 import {Link} from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
+
+
 
 const LoginForm = () => {
+    //Error usrname
+    const [errorUsr, setErrorUsr] = useState('');
+    //const [errorPass, setErrorPass] = useState('');
+    
 
     const showPassword = () =>{
         let pass = document.getElementById('password');
@@ -14,11 +21,32 @@ const LoginForm = () => {
         }   
     };
 
-    let navigate = useNavigate();
-    const submitHandler = ()=>{
-        navigate("/feed", {replace: true});
+    const validarForm = (event)=>{
+
+        //Previene que el formulario no se envie
+        event.preventDefault();
+
+        
+       //Creacion de variables para almacenar los datos en un obj
         let usr = document.getElementById('usrname').value;
-        let passw = document.getElementById('password').value;
+        let passw = document.getElementById('password').value;   
+
+        //const datos = {usrname:usr, password:passw}; 
+        //console.log(datos);
+
+        try {
+            const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({usrname:usr, password:passw})
+            };
+            fetch('http://localhost:9000/login', requestOptions)
+                .then(response => response.json());
+                //.then(data => element.innerHTML = data.id );
+        } catch (error) {
+            setErrorUsr('Nombre o usuario no valido');
+        }
+
     };
 
     return(
@@ -32,19 +60,21 @@ const LoginForm = () => {
                     <div className="container-title">
                         <h5>Ingresar</h5>
                     </div>
-                    <form className="loginForm">
+                    <form className="loginForm" onSubmit={validarForm}>
                         <p>Usuario</p>
                         <input type="text" className="form-inputs" id="usrname" placeholder="Usuario"/>
+                        { Boolean (errorUsr) && <p>Nombre de usuario o contraseña incorrectos</p>}
                         <p>Contraseña</p>
                         <div className="loginForm-password">
                             <input type="password" className="form-inputs" id="password" placeholder="Contraseña" />
                             <i class="bi bi-eye-fill" onClick={showPassword}></i>
                         </div>
+                        { Boolean (errorUsr) && <p>Error contraseña</p> }
                         <div className="row">
                             <a href="none" className="recoveryPass">Recuperar contraseña</a>  
                         </div>
                         <div className="form-row-continuar">
-                            <input type="submit" onSubmit={submitHandler} className="form-continuar" value="Acceder"/>                            
+                            <input type="submit" className="form-continuar" value="Acceder"/>                            
                         </div>
                     </form>
                     <div className="row">
